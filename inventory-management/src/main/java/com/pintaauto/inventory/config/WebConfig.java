0b5cc7d57740dece62efbc93ab8e29a20c95b123
DylanGReinosoT <dgreinoso@espe.edu.ec>
2026-06-04 00:23:34 -0500

    configuracion final

--- inventory-management/src/main/java/com/pintaauto/inventory/config/WebConfig.java
+++ inventory-management/src/main/java/com/pintaauto/inventory/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5174")
+                        .allowedOrigins("https://pint-auto.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
